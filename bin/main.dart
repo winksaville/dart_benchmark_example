5c72f6c527ab6fbb04a0e64967bf25b7ca635308
Wink Saville <wink@saville.com>
2020-04-21 13:54:10 -0700

    Simplify output of main.dart
    Remove EmptyBenchmark report: it adds no information.

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,5 @@ import 'package:benchmark_example/benchmark_example.dart';
 
 void main(List<String> arguments) {
   const EmptyBenchmark bmark = EmptyBenchmark('empty');
-  print('EmptyBenchmark report:');
   bmark.report();
 }
