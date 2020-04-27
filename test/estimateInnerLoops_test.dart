37b65f4986ae0f37bcd284ce9a3c987990d5906c
Wink Saville <wink@saville.com>
2020-04-27 13:57:05 -0700

    Format estimateInnerLoops_test
    And removed unused import

--- test/estimateInnerLoops_test.dart
+++ test/estimateInnerLoops_test.dart
@@ -1,18 +1,17 @@
-import 'dart:async' show TimeoutException;
-
 import 'package:benchmark_base_x/benchmarkbasex.dart';
 import 'package:test/test.dart';
 
 class BenchmarkDelayMicros extends BenchmarkBaseX {
-   BenchmarkDelayMicros(String name, this.delayMicros) : super(name);
+  BenchmarkDelayMicros(String name, this.delayMicros) : super(name);
 
   int delayMicros;
   Stopwatch watch = Stopwatch();
 
-  @override void run() {
+  @override
+  void run() {
     watch.reset();
     watch.start();
-    while(watch.elapsedMicroseconds < delayMicros) {}
+    while (watch.elapsedMicroseconds < delayMicros) {}
   }
 }
 
@@ -24,7 +23,8 @@ void main() {
     const double searchTimeInSecs = 0.250;
     const double executionTimeInSecs = 2.0;
     const int samples = 100;
-    final int innerLoops = bm.estimateInnerLoops(searchTimeInSecs, bm.exercise, executionTimeInSecs, samples);
+    final int innerLoops = bm.estimateInnerLoops(
+        searchTimeInSecs, bm.exercise, executionTimeInSecs, samples);
     bm.teardown();
 
     //print('innerLoops=$innerLoops');
